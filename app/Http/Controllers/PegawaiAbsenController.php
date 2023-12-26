2205100938440a5a20e0be5089f3e559344694a4
agungwahyu14 <wahyubrahmantha05@gmail.com>
2023-12-26 22:29:26 +0800

    error absensi begawai not support method post

--- app/Http/Controllers/PegawaiAbsenController.php
+++ app/Http/Controllers/PegawaiAbsenController.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use App\Models\Absensi;
 use Illuminate\Http\Request;
 
 class PegawaiAbsenController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
+        //
         $user = User::latest()->get();
         return view('pegawai.absen.index', compact(['user']),
             [
@@ -19,14 +23,20 @@ class PegawaiAbsenController extends Controller
             ]
         );
     }
-    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-    
         $request->validate([
             'id_pegawai' => 'required',
             'tanggal' => 'required',
@@ -43,5 +53,37 @@ class PegawaiAbsenController extends Controller
         ]);
 
         return redirect('/pegawai_home')->with('success', 'Absen has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
