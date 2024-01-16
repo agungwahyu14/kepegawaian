d98fa09ce29e1b261085d47bd6a4d78518ff6cb8
agungwahyu14 <wahyubrahmantha05@gmail.com>
2024-01-16 22:06:16 +0800

    fix landing page

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -1,52 +1,108 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title }}</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Sistem Informasi Kepegawaian</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+
+    <!-- CSS Libraries -->
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA -->
 </head>
 
-<body>
+<body class="layout-3">
+    <div id="app">
+        <div class="main-wrapper container-fluid">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <a href="index.html" class="navbar-brand ml-5 ">SIK</a>
+                
+                
+                <form class="form-inline ml-auto">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active"><a href="#" class="nav-link">Application</a></li>
+                        <li class="nav-item active"><a href="#" class="nav-link">Report Something</a></li>
+                        <li class="nav-item active"><a href="#" class="nav-link">Server Status</a></li>
+                    </ul>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-light ">
-        <div class="container">
-            <div class="login-brand">
-                <img src="assets/img/stisla-fill.svg" alt="logo" width="50" class="shadow-light rounded-circle ms-3">
-              </div>
+                </form>
 
-            <a class="nav-link {{ $title === 'About' ? 'active' : '' }} fw-medium text-primary-emphasis  ms-4"
-                aria-current="page" href="/about">About</a>
-
-            <a class="nav-link {{ $title === 'News' ? 'active' : '' }} fw-medium text-primary-emphasis ms-3"  "
-                href="/news">News</a>
-
-
-            <div class="collapse navbar-collapse " id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ $title === 'Login' ? 'active' : '' }} fw-bolder text-primary-emphasis"
-                            href="/login">Login</a>
-                    </li>
+            </nav>
 
 
-                </ul>
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section">
+
+
+                    <div class="section-body">
+                        <div class="row">
+                            
+                
+                            <div class="col-12 mb-4">
+                                <div class="hero text-white hero-bg-image"
+                                    style="background-image: url('assets/img/unsplash/eberhard-grossgasteiger-1207565-unsplash.jpg');">
+                                    <div class="hero-inner">
+                                        <h2>Welcome, !</h2>
+                                        <p class="lead">We're working hard to finish the development of this site. Sign in below to receive updates and to be notified when we launch!</p>
+                                        <div class="mt-4">
+                                            <a href="/login" class="btn btn-outline-white btn-lg btn-icon icon-left"><i
+                                                    class="far fa-user"></i> Login Account</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
         </div>
-    </nav>
     </div>
 
-    <div class="container">
-        @yield('conten')
-    </div>
+    <!-- General JS Scripts -->
+    <script src="assets/modules/jquery.min.js"></script>
+    <script src="assets/modules/popper.js"></script>
+    <script src="assets/modules/tooltip.js"></script>
+    <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="assets/modules/moment.min.js"></script>
+    <script src="assets/js/stisla.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-    </script>
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/custom.js"></script>
 </body>
 
 </html>
